176d1b1a9318ee7c408589cf559932668d15de05
V1adKry1ov <vlad.krylov2012@mail.ru>
2020-08-21 12:11:35 +0300

    webpack template

---
+++ src/components/Example.vue
@@ -0,0 +1,19 @@
+<template>
+    <section id="main">
+            <h1>Webpack Template</h1>
+            <img src="/assets/img/vue.jpg" style="width: 200px;">
+    </section>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+h1 {
+    text-align: center;
+    color: #333;
+}
+</style>
